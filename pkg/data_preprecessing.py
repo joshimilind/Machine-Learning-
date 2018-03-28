47e788b67815da0d57badf2a9e2e803e42091380
Milind <milind.joshi@synerzip.com>
2018-03-28 12:46:46 +0530

    reading data

---
+++ pkg/data_preprecessing.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# reading data
+dataset = pd.read_csv(
+    '/home/synerzip/nlp/nlp_workspace/Machine Learning/pkg/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
+# dataset = pd.read_csv('./Data.csv')
+print(dataset)
