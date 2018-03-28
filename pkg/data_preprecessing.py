9d1a337a4fcc14b85c27eb30c936de20f4ad148e
Milind <milind.joshi@synerzip.com>
2018-03-28 12:48:16 +0530

    creating fetaure matrix

--- pkg/data_preprecessing.py
+++ pkg/data_preprecessing.py
@@ -7,3 +7,10 @@ dataset = pd.read_csv(
     '/home/synerzip/nlp/nlp_workspace/Machine Learning/pkg/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
 # dataset = pd.read_csv('./Data.csv')
 print(dataset)
+
+# matrix of feature
+X = dataset.iloc[:, :-1].values
+print('x\n', X)
+
+Y = dataset.iloc[:, 3].values
+print('y\n', Y)
