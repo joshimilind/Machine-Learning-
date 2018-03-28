dc52bb25d8816ca890e9bfe87fffc1166b93b8ab
Milind <milind.joshi@synerzip.com>
2018-03-28 12:49:56 +0530

    done label encoding

--- pkg/data_preprecessing.py
+++ pkg/data_preprecessing.py
@@ -14,3 +14,24 @@ print('x\n', X)
 
 Y = dataset.iloc[:, 3].values
 print('y\n', Y)
+
+from sklearn.preprocessing import Imputer
+
+np.set_printoptions(threshold=np.nan)
+# handling missing data
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+
+# imputer = Imputer(missing_values='NaN', strategy='most_frequent', axis=0)
+# imputer = Imputer(missing_values='NaN', strategy='median', axis=0)
+
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print('x\n', X)
+
+# Encoding categorical variable
+from sklearn.preprocessing import LabelEncoder
+
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+
+print('\nfit transform\n', X)
