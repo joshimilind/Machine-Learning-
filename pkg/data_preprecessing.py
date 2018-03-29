aa3a3f6ce115367435d65e26d3182fb44d5873bb
Milind <milind.joshi@synerzip.com>
2018-03-29 14:50:38 +0530

    data preprocessing steps

--- pkg/data_preprecessing.py
+++ pkg/data_preprecessing.py
@@ -1,3 +1,4 @@
+# Data pre processing
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -46,3 +47,17 @@ Y = labelencoder_Y.fit_transform(Y)
 
 print('Y after label encoding>>\n', Y)
 
+# splitting data into test data and train data
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+
+print(X_train)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
