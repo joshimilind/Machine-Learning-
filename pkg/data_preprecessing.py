6751cfac0e43263ca47ee80ce37b7aa8254be707
Milind <milind.joshi@synerzip.com>
2018-03-29 14:53:28 +0530

    data preprocessing steps

--- pkg/data_preprecessing.py
+++ pkg/data_preprecessing.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # reading data
-dataset = pd.read_csv(
-    '/home/synerzip/nlp/nlp_workspace/Machine Learning/pkg/Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/Data.csv')
-# dataset = pd.read_csv('./Data.csv')
+
+dataset = pd.read_csv('./Data.csv')
 print(dataset)
 
 # matrix of feature
